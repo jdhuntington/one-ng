67c0667b7867552cdec190a95894743200fdf81e
JD Huntington <jdhuntington@gmail.com>
2014-09-29 17:53:41 -0700

    New ext

--- ext/onejs/oneavatar/OneAvatarModel.js
+++ ext/onejs/oneavatar/OneAvatarModel.js
@@ -9,18 +9,16 @@ define(["require", "exports", '../onejs/ViewModel', './md5'], function(require, 
         __extends(OneAvatarModel, _super);
         function OneAvatarModel() {
             _super.apply(this, arguments);
-            this.data = {
-                src: "#",
-                className: "c-oneavatar",
-                address: "uninitialized value",
-                size: 400
-            };
+            this.src = "#";
+            this.className = "c-oneavatar";
+            this.address = "uninitialized value";
+            this.size = 400;
         }
         OneAvatarModel.prototype.setData = function (data, shouldFireChange) {
             if (data && data.address) {
-                this.data.address = data.address;
+                this.address = data.address;
                 var hash = (new MD5()).md5(data.address);
-                this.data.src = "http://www.gravatar.com/avatar/" + hash + "?s=" + this.data.size;
+                this.src = "http://www.gravatar.com/avatar/" + hash + "?s=" + this.size;
                 this.change();
             }
             _super.prototype.setData.call(this, data, shouldFireChange);
