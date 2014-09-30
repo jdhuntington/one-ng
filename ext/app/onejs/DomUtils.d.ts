103d0d72af3d1983ccbe9b1ba55992058109d0cd
JD Huntington <jdhuntington@gmail.com>
2014-09-29 17:39:54 -0700

    remove old ext

--- ext/app/onejs/DomUtils.d.ts
+++
@@ -1,6 +0,0 @@
-declare class DomUtils {
-    static toggleClass(element: any, className: any, isEnabled: any): void;
-    static loadStyles(rules: any): void;
-    static setText(el: any, text: any): void;
-}
-export = DomUtils;
